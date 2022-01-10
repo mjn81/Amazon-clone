35bc661c384a3447b4fe47ec723410dfe721460b
mjn81 <mjjnn81@gmail.com>
2022-01-10 19:44:25 +0330

    some routes configed

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
 import './global/reset.css'
 import './global/utilities.css'
 import Home from './components/Home'
-import {BrowserRouter as Router , Route} from 'react-router-dom'
+import {BrowserRouter as Router , Route , Switch , Redirect} from 'react-router-dom'
 import React from 'react'
 
 
 function App() {
   return (
     <Router>
-      <Route>
-        <React.Fragment>
-          <Home />
-        </React.Fragment>
+      <Switch>
+        <Route path={'/'} exact>
+          <Redirect to={'/home'}></Redirect>
       </Route>
+      <Route path={'/home'}>
+        <Home />
+      </Route>
+      </Switch>
     </Router>
    
   );
