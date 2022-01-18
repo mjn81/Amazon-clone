3b157a86e3d3632496ebf5732a30f9f86c723b95
mjn81 <mjjnn81@gmail.com>
2022-01-18 20:19:54 +0330

    working on slider animation

--- src/components/slider/Slider.js
+++ src/components/slider/Slider.js
@@ -1,14 +1,30 @@
 import useFetch from "hooks/useFetch";
 import style from "./slider.module.css";
-
-const sliderFunction = ()=>{
-    const 
+import { useEffect } from "react";
+const sliderFunction = (index , total)=>{
+    const slides = document.querySelectorAll(`.${style.slide}`);
+    const pre = index-1 > -1 ? index-1 : total-1;
+    const nxt = index+1 < total ? index+1 : 0;
+    slides[pre].classList.remove(style.active); 
+    slides[index].classList.remove(style.nxt);
+    slides[nxt].classList.remove(style.pre);
+    slides[index].classList.add(style.active);
+    slides[pre].classList.add(style.pre);
+    slides[nxt].classList.add(style.nxt); 
+    setTimeout(()=>{
+        sliderFunction(nxt , total);
+    } , 7000)
 }
 
 
 const Slider = () => {
     const slides = useFetch('http://localhost:5000/slider');
-    
+    useEffect(()=>{
+          slides&&setTimeout(()=>{
+            sliderFunction(0 , slides.length);
+        } , 2000)
+    } , [slides]);
+   
     return (
         <div className={style.container}>
             <div className={style.slider}>
