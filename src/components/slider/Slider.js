ae2bcdd0442213c562965e96b8d19293e4d7a30e
mjn81 <mjjnn81@gmail.com>
2022-01-20 13:51:00 +0330

    some minor bugs fixed in Slider Component

--- src/components/slider/Slider.js
+++ src/components/slider/Slider.js
@@ -4,52 +4,69 @@ import { useEffect } from "react";
 import SliderBtn from "./SliderBtn";
 let pre , nxt , index, total;
 
+const nextSlide = (isLeft)=>{
+    if(isLeft){   
+        index = index-1 < 0 ? total-1 : index-1;
+    }else{
+        index = index+1> total-1 ? 0 : index+1;
+    }
+    
+}
 
-
-const move = (index , isLeft)=>{ 
-    pre = index-1 > -1 ? index-1 : total-1;
-    nxt = index+1 < total ? index+1 : 0;
+const move = (index , isLeft)=>{
+    nextSlide(isLeft);
     const slides = document.querySelectorAll(`.${style.slide}`);
+    if(isLeft){
+        pre = index+1 < total ? index+1 : 0;
+        nxt = index-1 > -1 ? index-1 : total-1;
+        slides[pre].classList.remove(style.for);
+        slides[nxt].classList.remove(style.for);
+        slides[pre].classList.add(style.back);
+        slides[nxt].classList.add(style.back);
+       
+    }else{
+        pre = index-1 > -1 ? index-1 : total-1;
+        nxt = index+1 < total ? index+1 : 0;
+        slides[pre].classList.remove(style.back);
+        slides[nxt].classList.remove(style.back);
+        slides[pre].classList.add(style.for);
+        slides[nxt].classList.add(style.for);
+    }
+  
     slides[pre].classList.remove(style.active); 
     slides[nxt].classList.remove(style.active); 
     slides[index].classList.remove(style.nxt);
-    slides[nxt].classList.remove(style.pre);
     slides[index].classList.remove(style.pre);
+    slides[nxt].classList.remove(style.pre);
     slides[nxt].classList.remove(style.nxt);
-    if(isLeft){
-        slides[index].classList.add(style.active);
-        slides[nxt].classList.add(style.pre);
-        slides[pre].classList.add(style.nxt); 
-    }else{
-        slides[index].classList.add(style.active);
-        slides[pre].classList.add(style.pre);
-        slides[nxt].classList.add(style.nxt); 
-    }
-    
+    slides[pre].classList.remove(style.pre); 
+    slides[pre].classList.remove(style.nxt); 
+
+    slides[index].classList.add(style.active);
+    slides[pre].classList.add(style.pre);
+    slides[nxt].classList.add(style.nxt); 
     
 }
 const circleFunction = ()=>{
-    index=nxt;
     move(index , false);
     setTimeout(()=>{
         circleFunction();
-    } , 4000)
+    } , 7000);
 }
 const toLeft = ()=>{
-    index=pre;
     move(index , true);
 }
 const toRight = ()=>{
-    index = nxt;
     move(index , false);
 }
 const Slider = () => {
     const slides = useFetch('http://localhost:5000/slider');
     useEffect(()=>{
-            index =0;
-            nxt=1;
-            pre=slides?.length-1;
+            index =1;
+            nxt=2;
+            pre=0;
             total = slides?.length;
+            total&&move(index  , true);
             slides&&setTimeout(()=>{
                 circleFunction();
             } , 2000);
@@ -64,7 +81,7 @@ const Slider = () => {
                 slides&&
                 slides.map(
                     (element)=>(
-                        <div key={element.id} className={style.slide}><img src={element.img} alt="" /></div>
+                        <div key={element.id} className={style.slide}><img src={element.img} alt={element.title} /></div>
                     )
                 )
             }
